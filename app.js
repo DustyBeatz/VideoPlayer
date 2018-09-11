b59a02a4393b2d4554d3a136ddc79e1a5b3110ac
Reece Devine <reece.devine97@gmail.com>
2018-09-11 12:52:22 -0300

    added new folder with buttons

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-console.log('first');
+doucment.addEventListener("DOMContentLoaded" , function() { startPlayer(); }, false);
+var player;
+
+function startPlayer()
+{
+    player = document.getElementById('videoPlayer');
+    player.controls = false;
+
+}
+
+function play_vid()
+{
+    player.play();
+}
+
+function pause_vid()
+{
+    player.pause();
+}
+
+function change_vol()
+{
+    player.volume=document.getElemtsById("change_vol").value;
+}
